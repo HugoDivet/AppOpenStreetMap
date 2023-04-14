f40e32960ffe190d06f4e1432e5ef79f2ee699e4
Gabrieldlb <gabriel.delabiche@epsi.fr>
2023-04-14 15:02:38 +0200

    flask init

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
+from flask import Flask, render_template_string
 import folium
 import requests
 
-row = 1
-url_api_tan = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_tan-arrets&rows=" + str(row)
+app = Flask(__name__)
 
-response = requests.get(url_api_tan)
 
-if response.status_code == 200:
-    data = response.json()
-else:
-    print("Erreur lors de la requête : ", response.status_code)
-print(data)
+@app.route("/")
+def fullscreen():
+    row = 100
+    url_api_tan = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_tan-arrets&rows=" + str(
+        row)
 
-m = folium.Map(location=[47.2301,-1.5429], zoom_start=13)
-for stop in data['records']:
-    if stop['fields']['location_type'] == '1':
-        folium.Marker(
-            location=stop['fields']['stop_coordinates'],
-            popup=stop['fields']['stop_name'],
-            icon=folium.Icon(icon="cloud")
-        ).add_to(m)
+    response = requests.get(url_api_tan)
 
-m.save('index.html')
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        print("Erreur lors de la requête : ", response.status_code)
+    print(data)
+
+    m = folium.Map(location=[47.2301, -1.5429], zoom_start=13)
+    for stop in data['records']:
+        if stop['fields']['location_type'] == '1':
+            folium.Marker(
+                location=stop['fields']['stop_coordinates'],
+                popup=stop['fields']['stop_name'],
+                icon=folium.Icon(icon="cloud")
+            ).add_to(m)
+
+    m.save('index.html')
+    return m.get_root().render()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
