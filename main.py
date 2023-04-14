2e4ba90f4a0356ee5f00a22038e4a08fe45f126b
M.Filiot <gabifiil68@gmail.com>
2023-04-14 13:21:27 +0200

    Add fetch of api nantes tan

---
+++ main.py
@@ -0,0 +1,24 @@
+import folium
+import requests
+
+row = 1
+url_api_tan = "https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_tan-arrets&rows=" + str(row)
+
+response = requests.get(url_api_tan)
+
+if response.status_code == 200:
+    data = response.json()
+else:
+    print("Erreur lors de la requête : ", response.status_code)
+print(data)
+
+m = folium.Map(location=[47.2301,-1.5429], zoom_start=13)
+for stop in data['records']:
+    if stop['fields']['location_type'] == '1':
+        folium.Marker(
+            location=stop['fields']['stop_coordinates'],
+            popup=stop['fields']['stop_name'],
+            icon=folium.Icon(icon="cloud")
+        ).add_to(m)
+
+m.save('index.html')
